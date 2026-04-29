6fdf374c1231e967e8ca310df58a1263abc2c63c
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-04-30 02:08:14 +0300

    feat: add footer

---
+++ src/components/layout/footer/footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './footer.module.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+export class Footer extends React.Component {
+  render() {
+    return (
+      <footer className={cx('footer')}>
+        <span className={cx('logo')}>RS School</span>
+        <a
+          className={cx('github')}
+          href="https://github.com/rika-milew"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Eryka Mileuskaya
+        </a>
+        <span className={cx('year')}>© {new Date().getFullYear()}</span>
+      </footer>
+    );
+  }
+}
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './layout.module.css';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 import classNames from 'classnames/bind';
 
@@ -18,6 +19,7 @@ export class Layout extends React.Component<Props> {
         <main className={cx('main')}>
           <div className={cx('container')}>{this.props.children}</div>
         </main>
+        <Footer />
       </div>
     );
   }
