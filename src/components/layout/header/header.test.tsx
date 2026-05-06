bc762426113581c0a64808c0b768e9e98489882b
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-05-07 00:10:07 +0300

    feat: add header and footer tests

---
+++ src/components/layout/header/header.test.tsx
@@ -0,0 +1,59 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect, vi, afterEach } from 'vitest';
+import { Header } from './header';
+import { waitFor } from '@testing-library/react';
+
+describe('Header component', () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('renders header content correctly', () => {
+    render(<Header />);
+
+    const header = screen.getByRole('banner');
+
+    expect(header).toBeInTheDocument();
+    expect(screen.getByText('RS')).toBeInTheDocument();
+    expect(screen.getByText('React App')).toBeInTheDocument();
+    expect(screen.getByText('React Class Components')).toBeInTheDocument();
+  });
+
+  it('has banner role', () => {
+    render(<Header />);
+
+    expect(screen.getByRole('banner')).toBeInTheDocument();
+  });
+
+  it('adds sticky class when scrollY is greater than threshold', async () => {
+    render(<Header />);
+
+    Object.defineProperty(globalThis, 'scrollY', {
+      value: 100,
+      writable: true,
+    });
+
+    globalThis.dispatchEvent(new Event('scroll'));
+    const header = screen.getByRole('banner');
+
+    await waitFor(() => {
+      expect(header.className).toMatch(/sticky/);
+    });
+  });
+
+  it('does not add sticky class when scrollY is smaller than scroll threshold', async () => {
+    render(<Header />);
+
+    Object.defineProperty(globalThis, 'scrollY', {
+      value: 2,
+      writable: true,
+    });
+
+    globalThis.dispatchEvent(new Event('scroll'));
+    const header = screen.getByRole('banner');
+
+    await waitFor(() => {
+      expect(header.className).not.toMatch(/sticky/);
+    });
+  });
+});
