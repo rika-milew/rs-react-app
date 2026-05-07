57689760d38d6b59e09ba82fbd545ab1fde98af9
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-05-07 22:36:05 +0300

    feat: edit error button test

--- src/components/error-button/error-button.test.tsx
+++ src/components/error-button/error-button.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { ErrorButton } from './error-button';
 import userEvent from '@testing-library/user-event';
 import { ErrorBoundary } from '@/components/error-boundary/error-boundary';
