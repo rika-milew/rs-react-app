a6b65e3ad2a7a73b5c9fa382c0781b9492d6e83f
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-05-08 12:17:57 +0300

    refactor: move setuptests to test-utils

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/test/setupTests.ts',
+    setupFiles: './src/test-utils/setupTests.ts',
 
     coverage: {
       reporter: ['text', 'html'],
@@ -21,7 +21,7 @@ export default defineConfig({
         'src/**/*.test.{js,jsx,ts,tsx}',
         'src/**/*.spec.{js,jsx,ts,tsx}',
         'src/index.{js,jsx,ts,tsx}',
-        'src/test/setupTests.{js,ts}',
+        'src/test-utils/setupTests.{js,ts}',
         'src/**/*.d.ts',
       ],
 
