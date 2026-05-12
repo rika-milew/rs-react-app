99ca4acc647643f30e182d43ec9cce9787e64bc7
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-05-12 22:20:02 +0300

    refactor: convert footer

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
 import styles from './footer.module.css';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-export class Footer extends React.Component {
-  public render() {
-    return (
-      <footer className={cx('footer')}>
-        <span className={cx('logo')}>RS School</span>
-        <a
-          className={cx('github')}
-          href="https://github.com/rika-milew"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Eryka Mileuskaya
-        </a>
-        <span className={cx('year')}>© {new Date().getFullYear()}</span>
-      </footer>
-    );
-  }
+export function Footer() {
+  return (
+    <footer className={cx('footer')}>
+      <span className={cx('logo')}>RS School</span>
+      <a
+        className={cx('github')}
+        href="https://github.com/rika-milew"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Eryka Mileuskaya
+      </a>
+      <span className={cx('year')}>© {new Date().getFullYear()}</span>
+    </footer>
+  );
 }
