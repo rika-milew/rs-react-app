a51159b56a317497c8f88c2bcaf7191c697b2470
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-05-12 22:31:12 +0300

    refactor: refactor loader

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-const cx = classNames.bind(styles);
 import styles from './loader.module.css';
+
 import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 type Props = {
   loading?: boolean;
 };
 
-export class Loader extends React.Component<Props> {
-  public render() {
-    if (this.props.loading === false) {
-      return null;
-    }
-    return (
-      <div className={cx('loader')}>
-        <div className={cx('spinner')} />
-        <span className={cx('text')}>Loading…</span>
-      </div>
-    );
+export function Loader({ loading = true }: Props) {
+  if (!loading) {
+    return null;
   }
+
+  return (
+    <div className={cx('loader')}>
+      <div className={cx('spinner')} />
+      <span className={cx('text')}>Loading…</span>
+    </div>
+  );
 }
