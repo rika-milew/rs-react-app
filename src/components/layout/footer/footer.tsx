ac03a88f76e022113544a16af9634c19e6eb7a86
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-05-18 01:38:38 +0300

    feat: edit classnames

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -14,7 +14,7 @@ export function Footer() {
         rel="noreferrer"
       >
         Eryka Mileuskaya
-        <span className="visually-hidden"> (Opens in new tab)</span>
+        <span className={cx('visually-hidden')}> (Opens in new tab)</span>
       </a>
       <span className={cx('year')}>© {new Date().getFullYear()}</span>
     </footer>
--- src/pages/about/about-page.tsx
+++ src/pages/about/about-page.tsx
@@ -18,7 +18,7 @@ export const AboutPage = () => {
           rel="noreferrer"
         >
           rika-milew
-          <span className="visually-hidden"> (Opens in new tab)</span>
+          <span className={cx('visually-hidden')}> (Opens in new tab)</span>
         </a>
       </p>
 
@@ -31,7 +31,7 @@ export const AboutPage = () => {
           rel="noreferrer"
         >
           RS School React Course
-          <span className="visually-hidden"> (Opens in new tab)</span>
+          <span className={cx('visually-hidden')}> (Opens in new tab)</span>
         </a>{' '}
         demonstrating routing, pagination, and master-detail architecture using
         TanStack Router.
