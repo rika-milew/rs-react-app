86697899635f54510512280db73564b698477a74
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-05-28 14:35:44 +0300

    chore: edit comma settings

--- eslint.config.js
+++ eslint.config.js
@@ -124,7 +124,7 @@ export default defineConfig([
       semi: ['error', 'always'],
       curly: ['error', 'all'],
       indent: ['error', 2, { SwitchCase: 1 }],
-      'comma-dangle': ['off'],
+      'comma-dangle': ['error', 'always-multiline'],
       'object-curly-spacing': ['error', 'always'],
       'brace-style': [
         'error',
