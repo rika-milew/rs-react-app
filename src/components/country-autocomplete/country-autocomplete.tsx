a40bf7d9b370c72e2f7d691211dd6104c8a72f57
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-08 00:41:59 +0300

    feat: add form field tests

--- src/components/country-autocomplete/country-autocomplete.tsx
+++ src/components/country-autocomplete/country-autocomplete.tsx
@@ -61,6 +61,7 @@ export function CountryAutocomplete({
         data-testid="hidden-input"
       />
       <input
+        id={name}
         type="text"
         value={inputValue}
         onChange={handleInput}
