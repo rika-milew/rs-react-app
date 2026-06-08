61be2fbffb5c9603169badb6e220b84d508af3fc
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-08 21:42:01 +0300

    feat: edit type guard

--- src/types/type-guards.ts
+++ src/types/type-guards.ts
@@ -169,7 +169,16 @@ export function isValidItemSpecies(data: unknown): data is PokemonSpecies {
 export function isFetchBaseQueryError(
   error: unknown,
 ): error is FetchBaseQueryError {
-  return typeof error === 'object' && error !== null && 'status' in error;
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'status' in error &&
+    (typeof error.status === 'number' ||
+      error.status === 'FETCH_ERROR' ||
+      error.status === 'PARSING_ERROR' ||
+      error.status === 'TIMEOUT_ERROR' ||
+      error.status === 'CUSTOM_ERROR')
+  );
 }
 
 export function isSerializedError(error: unknown): error is SerializedError {
