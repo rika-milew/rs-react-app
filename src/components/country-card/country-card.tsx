ae93fca3e6df4ba2d23d6cbbe4c7dd25aaff5227
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-11 00:04:23 +0300

    feat: optimize country card

--- src/components/country-card/country-card.tsx
+++ src/components/country-card/country-card.tsx
@@ -18,10 +18,12 @@ type CountryCardProps = {
 
 export const CountryCard = memo(({ country, selectedYear, selectedColumns }: CountryCardProps) => {
   const yearDataMap = useMemo(() => createYearDataMap(country.data), [country.data]);
+
   const population = useMemo(
     () => getPopulationForYear(yearDataMap, selectedYear),
     [yearDataMap, selectedYear]
   );
+
   const co2 = useMemo(() => getCo2ForYear(yearDataMap, selectedYear), [yearDataMap, selectedYear]);
 
   return (
