d369ed87ea54db91fa6af4c87f0ee5263e6f92d7
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-21 01:14:21 +0300

    feat: import react node

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { Provider } from 'react-redux';
+import type { ReactNode } from 'react';
 import { store } from '@/store';
 import { ThemeProvider } from '@/theme-context/theme-provider';
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }: { children: ReactNode }) {
   return (
     <Provider store={store}>
       <ThemeProvider>{children}</ThemeProvider>
