5b57c1929202562e10aec122943ea240627e2a15
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-22 15:58:26 +0300

    refactor: format code

--- eslint.config.js
+++ eslint.config.js
@@ -184,7 +184,7 @@ export default defineConfig([
   {
     files: ['src/i18n/request.ts'],
     rules: {
-    '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
     },
   },
   {
